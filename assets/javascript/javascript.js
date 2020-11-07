96cd969330b3142cd9ffc0559a7d706ddab1028d
alexeykuz-sys <alexeykuz@gmail.com>
2020-11-07 21:52:05 +0000

    edit language button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -20,3 +20,11 @@ jQuery(function ($) {
         display = $('#time');
     startTimer(oneMinute, display);
 });
+
+var dropdown = document.getElementById('d');
+var options = document.getElementById('o');
+var input = document.getElementById('v');
+
+dropdown.addEventListener('click', function(){
+    this.classList.toggle('show');
+})
