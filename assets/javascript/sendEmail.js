9f47c6b954ade2594bdfb99e69b25255f4b49b53
alexeykuz-sys <alexeykuz@gmail.com>
2020-12-06 14:30:43 +0000

    working on sendjs function

--- assets/javascript/sendEmail.js
+++ assets/javascript/sendEmail.js
@@ -1,20 +1,17 @@
-const btn = document.getElementById('button');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'default_service';
-   const templateID = 'linguagames';
-
-   emailjs.sendForm(serviceID, templateID, this)
+function sendMail(contactForm){
+    emailjs.send(email, linguagames, {
+        "from_name":contactForm.name.value,
+        "message":contactForm.message.value,
+        "from_email": contactForm.email.value,
+        
+    })
+    console.log(from_email, from_name, message)
     .then(() => {
       btn.value = 'Send Email';
+      console.log(btn.value)
       alert('Sent!');
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
     });
-});
+}
